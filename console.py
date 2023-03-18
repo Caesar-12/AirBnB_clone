7a1ee1e7d207357e1f703bcd1887404071082ae6
Caesar-12 <idowuenoch65@gmail.com>
2023-03-18 10:17:18 -0700

    Airbnb console with cmd module

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+contains the entry point of the command interpreter
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    CLI class
+    """
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, line):
+        """Quit command to exit the program\n"""
+        return True
+
+    def do_EOF(self, line):
+        """Quit command to exit the program\n"""
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
